fbc40f77f062d408a1fcf5626d791e92b9be73a3
lana shotashvili <shotashvililana@gmail.com>
2026-04-25 20:21:25 +0400

    feat: make HeroSection component reusable with props for title, paragraph, and link text

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,21 +2,17 @@ import "./HeroSection.css";
 import arrow from "../../assets/arrow.png";
 import sectionImage from "../../assets/section-image1.png";
 
-function HeroSection() {
+function HeroSection(props) {
   return (
     <>
       <section className="intro-section">
         <div className="left-section">
           <div className="left-section-info">
-            <h1>CREATE AND SHARE YOUR PHOTO STORIES</h1>
-            <p>
-              Photosnap is a platform for photographers and visual storytellers.
-              We make it easy to share photos, tell stories and connect with
-              others.
-            </p>
+            <h1>{props.title}</h1>
+            <p>{props.para}</p>
             <div className="invite-way">
               <a href="/invite" className="invite-link">
-                GET AN INVITE
+                {props.linkText}
               </a>
               <img src={arrow} alt="arrow" />
             </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,8 +4,14 @@ function Home() {
   return (
     <>
       <main>
-        <HeroSection />
-        <HeroSection />
+        <HeroSection
+          title="CREATE AND SHARE YOUR PHOTO STORIES"
+          para=" Photosnap is a platform for photographers and visual storytellers.
+              We make it easy to share photos, tell stories and connect with
+              others."
+          linkText="GET AN INVITE"
+        />
+        {/* <HeroSection /> */}
       </main>
     </>
   );
