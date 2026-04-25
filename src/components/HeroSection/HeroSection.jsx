4eaefdc7c7205e148c73ee70211657a27dca6cd5
lana shotashvili <shotashvililana@gmail.com>
2026-04-25 20:17:24 +0400

    refactor: remove main tag in HeroSection

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,29 +5,27 @@ import sectionImage from "../../assets/section-image1.png";
 function HeroSection() {
   return (
     <>
-      <main>
-        <section className="intro-section">
-          <div className="left-section">
-            <div className="left-section-info">
-              <h1>CREATE AND SHARE YOUR PHOTO STORIES</h1>
-              <p>
-                Photosnap is a platform for photographers and visual
-                storytellers. We make it easy to share photos, tell stories and
-                connect with others.
-              </p>
-              <div className="invite-way">
-                <a href="/invite" className="invite-link">
-                  GET AN INVITE
-                </a>
-                <img src={arrow} alt="arrow" />
-              </div>
+      <section className="intro-section">
+        <div className="left-section">
+          <div className="left-section-info">
+            <h1>CREATE AND SHARE YOUR PHOTO STORIES</h1>
+            <p>
+              Photosnap is a platform for photographers and visual storytellers.
+              We make it easy to share photos, tell stories and connect with
+              others.
+            </p>
+            <div className="invite-way">
+              <a href="/invite" className="invite-link">
+                GET AN INVITE
+              </a>
+              <img src={arrow} alt="arrow" />
             </div>
           </div>
-          <div className="right-section">
-            <img src={sectionImage} alt="section-image" />
-          </div>
-        </section>
-      </main>
+        </div>
+        <div className="right-section">
+          <img src={sectionImage} alt="section-image" />
+        </div>
+      </section>
     </>
   );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     <>
       <main>
         <HeroSection />
+        <HeroSection />
       </main>
     </>
   );
