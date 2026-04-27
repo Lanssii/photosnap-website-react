54f41beb434ec1c44ff34d0c438fc8a7e7769878
lana shotashvili <shotashvililana@gmail.com>
2026-04-27 18:47:58 +0400

    feat: start creating story-card for StorySection

--- src/components/StorySection/StorySection.jsx
+++ src/components/StorySection/StorySection.jsx
@@ -1,10 +1,23 @@
 import "./StorySection.css";
+import "../../assets/story-card1.png";
 
 function StorySection() {
   return (
     <>
       <section className="story-section">
-        <div className="story-card"></div>
+        <div className="story-card">
+          <div className="story-info">
+            <h4>The Mountains</h4>
+            <p>by John Appleseed</p>
+            <hr />
+            <div className="invite-way">
+              <a href="/invite" className="read-link">
+                READ STORY
+              </a>
+              <div className="arrow"></div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
