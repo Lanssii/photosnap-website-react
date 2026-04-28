08027f59d438ae88e875e3c5e13b700d1e4e9dc3
lana shotashvili <shotashvililana@gmail.com>
2026-04-28 19:09:32 +0400

    feat: start creating pricing page

--- src/views/Features.jsx
+++ src/views/Features.jsx
@@ -1,7 +1,18 @@
+import HeroSection from "../components/HeroSection/HeroSection.jsx";
+import InfoSection from "../components/InfoSection/InfoSection.jsx";
+import featuresImage from "../assets/features-intro.png";
+
 function Features() {
   return (
     <main>
-      <p>Features page</p>
+      <HeroSection
+        title="FEATURES"
+        para="We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories."
+        sectionImageOne={featuresImage}
+        backgroundColor="#000"
+      />
+      <InfoSection />
+      <InfoSection />
     </main>
   );
 }
--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -1,7 +1,15 @@
+import HeroSection from "../components/HeroSection/HeroSection.jsx";
+import pricingImage from "../assets/pricing-hero.png";
+
 function Pricing() {
   return (
     <main>
-      <p>Pricing page</p>
+      <HeroSection
+        title="PRICING"
+        para="Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos."
+        sectionImageOne={pricingImage}
+        backgroundColor="#000"
+      />
     </main>
   );
 }
