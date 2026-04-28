b31eedb258eb38083ca84f6e53ef2ed73852668d
lana shotashvili <shotashvililana@gmail.com>
2026-04-28 19:42:13 +0400

    feat: add toggle switch for pricing plan selection and update layout for PricingSection

--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -4,22 +4,36 @@ import PricingCard from "../PricingCard/PricingCard.jsx";
 function PricingSection() {
   return (
     <section className="pricing-section pricing-cards">
-      <PricingCard
-        title="Basic"
-        price="19"
-        desc="Includes basic usage of our platform. Recommended for new and aspiring photographers."
-      />
-      <PricingCard
-        title="Pro"
-        price="39"
-        desc="More advanced features available. Recommended for photography veterans and professionals."
-        isActive={true}
-      />
-      <PricingCard
-        title="Business"
-        price="99"
-        desc="Additional features available such as more detailed metrics. Recommended for business owners."
-      />
+      <div className="price-button">
+        <div className="toggle-wrapper">
+          <span className="label active">Monthly</span>
+
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider"></span>
+          </label>
+
+          <span className="label">Yearly</span>
+        </div>
+      </div>
+      <div className="price-cards">
+        <PricingCard
+          title="Basic"
+          price="19"
+          desc="Includes basic usage of our platform. Recommended for new and aspiring photographers."
+        />
+        <PricingCard
+          title="Pro"
+          price="39"
+          desc="More advanced features available. Recommended for photography veterans and professionals."
+          isActive={true}
+        />
+        <PricingCard
+          title="Business"
+          price="99"
+          desc="Additional features available such as more detailed metrics. Recommended for business owners."
+        />
+      </div>
     </section>
   );
 }
