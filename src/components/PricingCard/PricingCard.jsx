8ca5764b9f33d995bb82d66a2105aae4dcbace95
lana shotashvili <shotashvililana@gmail.com>
2026-04-28 19:31:15 +0400

    feat: create pricing cards

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -3,11 +3,13 @@ import "./PricingCard.css";
 function PricingCard({ title, price, desc, isActive }) {
   return (
     <div className={`pricing-card ${isActive ? "active" : ""}`}>
-      <h3>{title}</h3>
+      <h2>{title}</h2>
       <p>{desc}</p>
 
-      <h2 className="price">${price}.00</h2>
-      <span>per month</span>
+      <div className="price-offer">
+        <h2 className="price">${price}.00</h2>
+        <span>per month</span>
+      </div>
 
       <button className="btn">PICK PLAN</button>
     </div>
--- src/components/PricingSection/PricingSection.jsx
+++ src/components/PricingSection/PricingSection.jsx
@@ -4,14 +4,22 @@ import PricingCard from "../PricingCard/PricingCard.jsx";
 function PricingSection() {
   return (
     <section className="pricing-section pricing-cards">
-      <PricingCard title="Basic" price="19" desc="Includes basic usage..." />
+      <PricingCard
+        title="Basic"
+        price="19"
+        desc="Includes basic usage of our platform. Recommended for new and aspiring photographers."
+      />
       <PricingCard
         title="Pro"
         price="39"
-        desc="More advanced features..."
+        desc="More advanced features available. Recommended for photography veterans and professionals."
         isActive={true}
       />
-      <PricingCard title="Business" price="99" desc="Additional features..." />
+      <PricingCard
+        title="Business"
+        price="99"
+        desc="Additional features available such as more detailed metrics. Recommended for business owners."
+      />
     </section>
   );
 }
